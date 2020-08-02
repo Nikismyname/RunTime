cc4f35c81651b6f68fa0c3d2d2ef8410b6f94046
Nikismyname <nikismyname@abv.bg>
2020-08-02 13:59:48 +0300

    Clean up!

--- Src/Assets/Scripts/Game/05Levels/LevelPCT/ConstructLevel.cs
+++ Src/Assets/Scripts/Game/05Levels/LevelPCT/ConstructLevel.cs
@@ -3,31 +3,31 @@ using UnityEngine;
 
 public class ConstructLevel : MonoBehaviour
 {
-    public List<GameObject> allObjs;
+    //public List<GameObject> allObjs;
 
-    private void Start()
-    {
-        this.allObjs = new List<GameObject>();
+    //private void Start()
+    //{
+    //    this.allObjs = new List<GameObject>();
 
-        var data = ObjectSerialising.DeserialiseObjects("","");
+    //    var data = ObjectSerialising.DeserialiseObjects("","");
 
-        foreach (var dataPint in data)
-        {
-            this.CreateObj(dataPint);
-        }
-    }
+    //    foreach (var dataPint in data)
+    //    {
+    //        this.CreateObj(dataPint);
+    //    }
+    //}
 
-    private void CreateObj(PrimitiveObjectSerialiseData data)
-    {
-        var go = GameObject.CreatePrimitive(data.type);
-        go.transform.position = data.position;
-        go.transform.localScale = data.scale;
-        go.transform.eulerAngles = data.rotation;
-        go.GetComponent<Renderer>().material.color = data.color;
+    //private void CreateObj(PrimitiveObjectSerialiseData data)
+    //{
+    //    var go = GameObject.CreatePrimitive(data.type);
+    //    go.transform.position = data.position;
+    //    go.transform.localScale = data.scale;
+    //    go.transform.eulerAngles = data.rotation;
+    //    go.GetComponent<Renderer>().material.color = data.color;
 
-        var dm = go.AddComponent<PrimitiveObjectDataModifier>(); 
+    //    var dm = go.AddComponent<PrimitiveObjectDataModifier>(); 
 
-        this.allObjs.Add(go);
-    }
+    //    this.allObjs.Add(go);
+    //}
 }
 
--- Src/Assets/Scripts/Game/Purgatory/HubCustomising/Undo/IUndoItem.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public interface IUndoItem
-{
-    void Revert(PrimitiveObjectDataModifier pdom); 
-}
-
